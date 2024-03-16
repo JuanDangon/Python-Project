7d03ce9cda5be327b2837268cd5595edd8bb3ae5
Jake Tyler <jaketyler@Jakes-MacBook-Pro.local>
2024-03-16 15:33:41 -0500

    added Users table creation

---
+++ setup.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('SiteData.db')
+cursor = conn.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS Users (Username TEXT,Password TEXT,CreationDate DATETIME,FirstName TEXT,LastName TEXT, Email TEXT, Phone INTEGER, DOB DATETIME)')
+
+conn.commit()
+conn.close()

881db9733688e321b7f5ea0ad3db005bb4181e2a
ok20012 <90343026+ok20012@users.noreply.github.com>
2024-04-23 01:58:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,41 @@
 import sqlite3
 
 
-def setup():
-    user_table_statement = """CREATE TABLE IF NOT EXISTS Users (
-                            UserId INTEGER PRIMARY KEY,
-                            Username TEXT,
+# def setup():
+user_table_statement = """CREATE TABLE IF NOT EXISTS Users(
+                            UserID INTEGER PRIMARY KEY,
+                            Username TEXT, 
                             Password TEXT,
+                            CreationDate DATETIME,
+                            FirstNam TEXT, 
+                            LastName TEXT, 
+                            Email TEXT, 
+                            Phone INTEGER,
+                            DOB DATETIME, 
+                            SSN INTEGER)"""
+
+account_info_statement = """CREATE TABLE IF NOT EXISTS AccountInfo(
+                            Account INTEGER PRIMARY KEY,
+                            AccessToken INTEGER,
                             FirstName TEXT,
                             LastName TEXT,
-                            DOB TEXT,
-                            Email TEXT,
-                            PhoneNumber TEXT"""
+                            AccountName TEXT,
+                            CreationDate DATETIME,
+                            PostCount INTEGER,
+                            FollowerCount INTEGER,
+                            FollowingCount INTEGER,
+                            """
 
-    conn = sqlite3.connect('userData.db')
+conn = sqlite3.connect('userData.db')
 
+try:
     cursor = conn.cursor()
     cursor.execute(user_table_statement)
-    # Do whatever Table Creation Here
+    cursor.execute(account_info_statement)
+except sqlite3.Error as e:
+    print("Error while creating table:\n", e)
+# Do whatever Table Creation Here
 
-    conn.commit()
-    conn.close()
+conn.commit()
+conn.close()
+

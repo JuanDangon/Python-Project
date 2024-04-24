791e619799ce42352486fc5a4a1a12eb0897b03d
Trevor <trevordcooley@gmail.com>
2024-04-23 20:19:52 -0400

    added roles table

--- setup.py
+++ setup.py
@@ -26,6 +26,20 @@ account_info_statement = """CREATE TABLE IF NOT EXISTS AccountInfo(
                             FollowingCount INTEGER,
                             """
 
+roles_table_statement = """CREATE TABLE IF NOT EXISTS Roles (
+                            RoleID INTEGER PRIMARY KEY,
+                            RoleName TEXT UNIQUE
+                            )"""
+
+user_roles_statement = """CREATE TABLE IF NOT EXISTS UserRoles (
+                            UserID INTEGER,
+                            RoleID INTEGER,
+                            FOREIGN KEY(UserID) REFERENCES Users(UserID),
+                            FOREIGN KEY(RoleID) REFERENCES Roles(RoleID),
+                            PRIMARY KEY (UserID, RoleID)
+                            )"""
+
+
 conn = sqlite3.connect('userData.db')
 
 try:
@@ -35,6 +49,13 @@ try:
 except sqlite3.Error as e:
     print("Error while creating table:\n", e)
 # Do whatever Table Creation Here
+
+# adds admin role 
+cursor.execute("INSERT INTO Roles (RoleName) VALUES (?)", ('admin',))
+# adds regular user role 
+cursor.execute("INSERT INTO Roles (RoleName) VALUES (?)", ('regUser',))
+# adds premium user role 
+cursor.execute("INSERT INTO Roles (RoleName) VALUES (?)", ('premiumUser',))
 
 conn.commit()
 conn.close()
